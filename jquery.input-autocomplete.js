dee80331250cd39f7c16bd71398e723164ce8220
Brandyn Britton <Brandynbb96@gmail.com>
2020-02-06 08:36:51 +0800

    Quickfix to remove the border shown when initialized

--- jquery.input-autocomplete.js
+++ jquery.input-autocomplete.js
@@ -7,43 +7,43 @@
  */
 
 
-(function($) {
+(function ($) {
 
     // Keep an instance of the auto complete instances
     let inputAutocompleteInstanceId = 0;
 
     const defaults = {
-        predictableText : [],        // List of words to use for predictions
+        predictableText: [],        // List of words to use for predictions
 
         // Callbacks
-        onLoad          : function(element) {},
-        onAutocomplete  : function(element, value) {}
+        onLoad: function (element) { },
+        onAutocomplete: function (element, value) { }
     }
 
-    
+
     //  Polyfill to allow the insertion of text at a certain index
     String.prototype.insert = function (index, string) {
         if (index > 0)
-          return this.substring(0, index) + string + this.substring(index, this.length);
-        
-        return string + this;
-      };
+            return this.substring(0, index) + string + this.substring(index, this.length);
 
-      // Polyfill to allow the removal of characters from begin to end indexz
-      String.prototype.remainderOfSlice = function(begin, end) {
+        return string + this;
+    };
+
+    // Polyfill to allow the removal of characters from begin to end indexz
+    String.prototype.remainderOfSlice = function (begin, end) {
 
         begin = begin || 0
-        end = (end === undefined) ? this.length : end 
-    
+        end = (end === undefined) ? this.length : end
+
         if (this.slice(begin, end) === '') return this + ''
-        return this.slice(0, begin) + this.slice(end) 
-     }
+        return this.slice(0, begin) + this.slice(end)
+    }
 
     function InputAutocomplete(element, options) {
-        this.element                = element;
-        this.options                = $.extend(true, {}, defaults, options)
-        this.searchText             = [];
-        this.instanceId             = inputAutocompleteInstanceId;
+        this.element = element;
+        this.options = $.extend(true, {}, defaults, options)
+        this.searchText = [];
+        this.instanceId = inputAutocompleteInstanceId;
         this.currentlySelectedIndex = 0;
 
         // Load the autocomplete instance for this element
@@ -52,30 +52,33 @@
 
     InputAutocomplete.prototype = {
 
-        load: function() {
-            
+        load: function () {
+
             // Assume new instance is created when load  is called
             inputAutocompleteInstanceId += 1;
             var instance = this;
-            
+
             // Build the dropdown container
             this.buildDropdown();
 
-            $(instance.element).blur(function() {
+            // Hide the input initially until focused to prevent the border from showing
+            $(instance.element).siblings('.autocompleteList').hide();
+
+            $(instance.element).blur(function () {
                 $(instance.element).siblings('.autocompleteList').hide();
             });
 
-            $(instance.element).focus(function() {
+            $(instance.element).focus(function () {
                 $(instance.element).siblings('.autocompleteList').show();
             });
 
             // Listen to keyup events whenever the instance is focused and determine how to handle that
             let wordHasChanged = false;
-            let oldWordMeta = {word: ""};
-            $(instance.element).keyup(function(event) {
+            let oldWordMeta = { word: "" };
+            $(instance.element).keyup(function (event) {
 
                 // Get the word info under the cursor
-                let caretPos = $(instance.element)[0].selectionStart; 
+                let caretPos = $(instance.element)[0].selectionStart;
                 let currentWordMeta = instance.getWordUnderCaret(caretPos);
 
                 // Need to keep track of whether the word has changed so we can perform the appropriate actions
@@ -92,12 +95,12 @@
                     for (let i = 0; i < instance.options.predictableText.length; i++) {
                         if (instance.options.predictableText[i].toLowerCase().includes(currentWordMeta.word.toLowerCase())) {
                             instance.searchText.push(instance.options.predictableText[i]);
-                        } 
+                        }
                     }
 
                     // Build the new list elements
                     let wordPredictionListElements = "";
-                    for(let j = 0; j < instance.searchText.length; j++ ) {
+                    for (let j = 0; j < instance.searchText.length; j++) {
                         const newListElement = "<li>" + instance.searchText[j] + "</li>"
                         wordPredictionListElements += newListElement;
                     }
@@ -106,7 +109,9 @@
                     instance.updateAutocompleteList(instance);
 
                     instance.currentlySelectedIndex = 0;
-                } else {
+                }
+                else 
+                {
                     if (event.which === 38) {
                         instance.currentlySelectedIndex -= 1;
                         if (instance.currentlySelectedIndex < 0) instance.currentlySelectedIndex = 0;
@@ -121,7 +126,7 @@
                             const newInputValue = currentInputValue.remainderOfSlice(oldWordMeta.start, oldWordMeta.end).insert(oldWordMeta.caretPos, instance.searchText[instance.currentlySelectedIndex]);
                             $(instance.element).val(newInputValue);
                         }
-                    }    
+                    }
 
                     instance.updateAutocompleteList(instance);
                 }
@@ -129,7 +134,7 @@
             });
         },
 
-        updateAutocompleteList: function(instance) {
+        updateAutocompleteList: function (instance) {
 
             // Apply the selected attribute to the currently selected element
             const listElement = $(instance.element).siblings('.autocompleteList');
@@ -138,11 +143,11 @@
 
             // Need to refresh the onclick listeners for individual list elements because they have been refreshed
             $(".autocompleteList li").on({
-                mouseenter: function() {
+                mouseenter: function () {
                     instance.currentlySelectedIndex = $(this).index();
                     $(this).addClass("selected");
                 },
-                mouseleave: function() {
+                mouseleave: function () {
                     instance.currentlySelectedIndex = 0;
                     $(this).removeClass("selected");
                 }
@@ -157,18 +162,18 @@
         /**
          * Builds the dropdown container for the autocomplete instance
          */
-        buildDropdown: function() {
+        buildDropdown: function () {
 
             const instance = this;
 
             $(this.element).after("<ul class='autocompleteList' id='autocompleteList" + this.instanceId + "'></ul>");
-            
-            
+
+
 
             // I like dropdowns that match the original input box so position accordingly
             $(".autocompleteList").css({
                 "position": "absolute",
-                "width": $(this.element).innerWidth(), 
+                "width": $(this.element).innerWidth(),
                 "top": $(this.element).position().bottom,
                 "left": $(this.element).position().left,
                 "right": $(this.element).position().right,
@@ -186,26 +191,25 @@
          * 
          * @param {number} caretPos Position of the caret within the input
          */
-        getWordUnderCaret: function(caretPos) {
+        getWordUnderCaret: function (caretPos) {
 
             const currentVal = $(this.element).val();
-            
+
             // Branch to the left of the current index until we hit a space or start of input
             // Assume this is the start point of the current word
             let startIndex = caretPos;
             while (true) {
-                if (startIndex === 0)
-                {
+                if (startIndex === 0) {
                     break;
                 }
-                else if (currentVal[startIndex] === " "){
+                else if (currentVal[startIndex] === " ") {
                     startIndex += 1;
                     break;
-                }  
+                }
 
                 startIndex -= 1;
             }
-    
+
             // Branch to the right of the current index unti we hit a space or end of input
             // Assume this is the end of the current word
             let endIndex = caretPos;
@@ -221,19 +225,19 @@
             const word = currentVal.substring(startIndex, endIndex);
             word ? $.trim(word) : null;
 
-            return {start: startIndex, end: endIndex, caretPos: caretPos, word: word};
+            return { start: startIndex, end: endIndex, caretPos: caretPos, word: word };
         },
     };
 
-    
 
-    $.fn.inputAutocomplete = function(options) {
+
+    $.fn.inputAutocomplete = function (options) {
         if (!this.length) {
             return;
         }
 
         if ((options === undefined) || (typeof options === 'object')) {
-            return this.each(function() {
+            return this.each(function () {
                 if (!$.data(this, 'plugin_inputAutocomplete')) {
                     $.data(this, 'plugin_inputAutocomplete', new InputAutocomplete(this, options));
                 }
